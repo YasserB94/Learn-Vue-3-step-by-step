f00ba29e384a42dec1dec779a4b6d75d19c88484
Yasser Bal <yasserbal@icloud.com>
2022-11-20 19:38:04 +0100

    Component Props

--- getting-started/js/components/AppButton.js
+++ getting-started/js/components/AppButton.js
@@ -1,19 +1,30 @@
 export default {
     template: `
                 <button
-                class="duration-300
-                shadow rounded-xl bg-slate-200 border border-slate-500 text-slate-600 px-4 py-2
-                hover:text-emerald-600 hover:border-emerald-600 hover:bg-slate-300 active:scale-90
-                disabled:opacity-50 disabled:cursor-not-allowed"
+                :class="{
+                    'border border-slate-600 rounded px-4 py-2 text-slate-500':true,
+                    'disabled:cursor-not-allowed ':true,
+                    'hover:scale-105 active:scale-95':true,
+                    'bg-slate-200': type=== 'primary'||type==='secondary',
+                    'hover:bg-slate-500 hover:text-slate-200':type==='primary',
+                    'hover:bg-transparent hover:text-slate-50':type==='secondary',
+                    'hover:bg-emerald-800 text-slate-100':type==='accent',
+                    'text-transparent hover:text-transparent animate-pulse rounded-full':processing,
+                }"
                 :disabled="processing"
                 >
                 <slot />
                 </button>
                 `
     ,
-    data(){
-        return {
-            processing:false
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        processing: {
+            type: Boolean,
+            default: false
         }
     },
     mounted() {
