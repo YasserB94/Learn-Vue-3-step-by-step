48b6c14e3efefb760de72e7fb231faa7797dc670
YasserB94 <yasserbal@icloud.com>
2022-11-21 12:19:30 +0100

    Two Mental Leaps to Script Setup

--- my-first-vue-project/src/views/ContactView.vue
+++ my-first-vue-project/src/views/ContactView.vue
@@ -1,11 +1,31 @@
-<script>
-export default {
-  name: "ContactView",
-};
-</script>
+<script setup>
+import {ref} from "vue";
 
+const greeting = ref("Hello Composition API - Ref")
+let i;
+const resetInputOnInterval = () => {
+  if (i != null) {
+    return;
+  }
+  i = setInterval(() => {
+    greeting.value = 'Hello Composition API - Ref'
+  }, 3000)
+
+}
+</script>
 <template>
-  <h1 className="text-3xl font-bold underline">Hello Contact,Tailwind!</h1>
+  <h1>Hello Contact</h1>
+  <p className="text-3xl font-bold underline">Hello Tailwind!</p>
+  <div class="my-2 border p-2 rounded w-fit space-y-2">
+    <p>{{ greeting }}</p>
+    <input class="p-1 text-slate-800"
+           type="text"
+           @change="resetInputOnInterval"
+           v-model="greeting"
+    >
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
