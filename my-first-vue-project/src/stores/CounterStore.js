780b0a56ad5648f7fdad2647a33e2f0357eaaeeb
YasserB94 <yasserbal@icloud.com>
2022-11-22 12:08:17 +0100

    Say Hello to Pinia

--- my-first-vue-project/src/stores/CounterStore.js
+++ my-first-vue-project/src/stores/CounterStore.js
@@ -1,11 +1,34 @@
-import {reactive} from "vue";
-export let counter = reactive({
-    count:0,
-
-    increment(){
-        this.count++
+import { defineStore } from "pinia";
+const MAX_COUNT_VALUE = 3;
+const MIN_COUNT_VALUE = -3;
+export const useCounterStore = defineStore("counter", {
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  actions: {
+    increment() {
+      if (this.count + 1 > MAX_COUNT_VALUE) return;
+      this.count++;
     },
-    decrement(){
-        this.count--
+    decrement() {
+      if (this.count - 1 < MIN_COUNT_VALUE) return;
+      this.count--;
+    },
+  },
+  getters: {
+    remainingIncrementRange() {
+      return MAX_COUNT_VALUE - this.count;
+    },
+    hasRemainingIncrementRange(){
+      return (this.count===MAX_COUNT_VALUE)
+    },
+    remainingDecrementRange() {
+      return MIN_COUNT_VALUE - this.count;
+    },
+    hasRemainingDecrementRange(){
+      return (this.count===MIN_COUNT_VALUE)
     }
-})
+  },
+});
--- my-first-vue-project/src/views/CounterView.vue
+++ my-first-vue-project/src/views/CounterView.vue
@@ -1,26 +1,38 @@
 <script setup>
-import {counter} from "../stores/counterStore";
+import { useCounterStore } from "@/stores/CounterStore";
 
-
+const counter = useCounterStore();
 </script>
 
 <template>
   <div>
-    <p>
-      Counter View
-    </p>
-    <div class="grid grid-cols-2 grid-rows-2 border border-slate-300 rounded-xl py-2 m-2">
-    <p class="col-span-2 text-center">Count : {{counter.count}}</p>
-    <button
-        class="p-2 m-1 bg-slate-300 duration-500 text-slate-800 w-10 mx-auto rounded-full hover:scale-105 active:scale-90"
-        @click="counter.increment()">&#8593;</button>
-    <button
-        class="p-2 m-1 bg-slate-300 duration-500 text-slate-800 w-10 mx-auto rounded-full hover:scale-105 active:scale-90"
-        @click="counter.decrement()">&#8595;</button>
+    <p class="my-2 text-center uppercase">Pinia Counter</p>
+    <div
+      class="m-2 grid grid-cols-2 grid-rows-2 rounded-xl border border-slate-300 py-2"
+    >
+      <p class="col-span-2 my-auto text-center">Count : {{ counter.count }}</p>
+      <div class="row-span-1 mx-auto grid items-center">
+        <p class="w-10 text-center">{{ counter.remainingIncrementRange }}</p>
+        <button
+          class="w-10 rounded-full bg-slate-300 p-2 text-slate-800 duration-500 hover:scale-105 active:scale-90 disabled:text-transparent disabled:scale-150 disabled:opacity-0"
+          @click="counter.increment()"
+          :disabled="counter.hasRemainingIncrementRange"
+        >
+          &#8593;
+        </button>
+      </div>
+      <div class="mx-auto grid items-center">
+        <p class="w-10 text-center">{{ Math.abs(counter.remainingDecrementRange) }}</p>
+        <button
+          class="w-10 rounded-full bg-slate-300 p-2 text-slate-800 duration-500 hover:scale-105 active:scale-90 disabled:text-transparent disabled:scale-150 disabled:opacity-0"
+          :disabled="counter.hasRemainingDecrementRange"
+          @click="counter.decrement()"
+        >
+          &#8595;
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
