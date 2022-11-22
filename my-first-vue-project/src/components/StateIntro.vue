4cd1c7708a275fb16ef9726c61ae5f85e96a7775
YasserB94 <yasserbal@icloud.com>
2022-11-22 10:41:23 +0100

    Store State in an External File

---
+++ my-first-vue-project/src/components/StateIntro.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import {state} from "@/stores/quizStore";
+
+
+const changeState = ()=>{
+  state.name += "+";
+}
+
+</script>
+
+<template>
+  <p>{{state.name}}</p>
+  <button @click="changeState"
+  class="bg-slate-100 rounded-md text-slate-800 py-1 px-2 active:scale-95"
+  >Add a +</button>
+</template>
+
+<style scoped>
+
+</style>
--- my-first-vue-project/src/views/AboutView.vue
+++ my-first-vue-project/src/views/AboutView.vue
@@ -1,18 +1,21 @@
 <script setup>
 import {ref} from "vue";
-import IndentableTextarea from "@/components/IndentableTextarea.vue"
+import IndentableTextarea from "@/components/IndentableTextarea.vue";
 const comment = ref('Test')
+
+
+import StateIntro from '@/components/StateIntro.vue';
 </script>
 <template>
-  <div>
-    <h1>This is an about page</h1>
-    <IndentableTextarea
-        v-model="comment"
-        class="bg-white w-full h-40 resize-none rounded p-2 m-2 text-slate-800 font-mono text-xs"
-    ></IndentableTextarea>
-  </div>
+<!--  <div>-->
+<!--    <h1>This is an about page</h1>-->
+<!--    <IndentableTextarea-->
+<!--        v-model="comment"-->
+<!--        class="bg-amber-50 w-full h-40 resize-none rounded p-2 m-2 text-amber-900 font-mono text-xs"-->
+<!--    ></IndentableTextarea>-->
+<!--  </div>-->
+<StateIntro />
 </template>
-
 <style>
 
 </style>
